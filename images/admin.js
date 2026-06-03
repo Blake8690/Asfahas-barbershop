2019dbd1c014e6a8cf90077dfae4af90dc7ef836
Blake Joeseph <din-email-hr>
2026-06-03 11:28:02 +0200

    Update full styling with admin panel

--- images/admin.js
+++ images/admin.js
@@ -1,10 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+
 import {
     getAuth,
     signInWithEmailAndPassword,
     signOut,
     onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
 import {
     getFirestore,
     collection,
